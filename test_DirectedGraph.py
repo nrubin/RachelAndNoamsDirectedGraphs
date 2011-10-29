f19e8f3c346650693db944a7b5ff1484aa5aeb31
rachelbobbins <rachelheidi@gmail.com>
2011-10-29 15:37:53 -0400

    More tests

--- test_DirectedGraph.py
+++ test_DirectedGraph.py
@@ -2,16 +2,32 @@ import unittest
 from DirectedGraph import DirectedGraph, DirectedEdge, Vertex
 
 class TestDirectedGraph(unittest.TestCase):
-    def test_reflexive(self):
+    
+    def test_graph_inverse_correspondance(self):
+        """Does an edge FROM V to W get added to proper dictionaries?"""
         v = Vertex('v')
         w = Vertex('w')
         e = DirectedEdge(v, w)
 
-        dg = DirectedGraph([v,w], [e])
+        dg = DirectedGraph([v, w], [e])
         self.assertEqual(dg[v][w], e)
         self.assertEqual(dg[w], {})
         
         self.assertEqual(dg.inverse_graph[w][v], e)
+        self.assertEqual(dg.inverse_graph[v], {})
+
+    def test_remove_edge(self):
+        v = Vertex('v')
+        w = Vertex('w')
+        e = DirectedEdge(v, w)
+
+        dg = DirectedGraph([v, w], [e])
+
+        dg.remove_edge(v, w)
+
+        self.assertEqual(dg[v],{})
+        self.assertEqual(dg.inverse_graph[w],{})
+        self.assertEqual(dg[w], {})
         self.assertEqual(dg.inverse_graph[v], {})
 
 if __name__== "__main__":
