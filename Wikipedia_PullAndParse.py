9c0fb0770d79ca12041f8ab1aa9649d19aa17187
Noam Rubin <noam.rubin@students.olin.edu>
2011-12-02 14:51:08 -0500

    we made a graph of wikipediacompmod

--- Wikipedia_PullAndParse.py
+++ Wikipedia_PullAndParse.py
@@ -2,7 +2,8 @@ from BeautifulSoup import BeautifulSoup
 import pickle
 import os
 import urllib2
-
+from DirectedGraph import DirectedGraph, Arc, Vertex
+from DirectedGraphWorld import show_directed_graph
 
 def getUrls(url):
     """Returns a list of URLS linked to from the given page"""
@@ -30,5 +31,25 @@ def getLimitedUrls(parent, url_cache):
 
 index_url = 'http://en.wikipedia.org/wiki/Index_of_neurobiology_articles'
 url_cache = getUrls(index_url)
+
+#graph stuff
+vertices = [Vertex(url) for url in url_cache]
+dg = DirectedGraph(vertices, [])
+url_dict={}
+print dg.vertices()
 for url in url_cache:
-    print getLimitedUrls(url, url_cache)
+    url_dict[url] = getLimitedUrls(url, url_cache)
+    for out in url_dict[url]:
+        a = Arc(Vertex(url), Vertex(out))
+        dg.add_arc(a)
+   
+#~ dgw = DirectedGraphWorld()
+show_directed_graph(dg)
+
+#~ test_results = set()
+#~ for key, value in url_dict.items():
+    #~ for item in value:
+        #~ test_results.add(item)
+        #~ 
+#~ print test_results
+#~ print set(url_cache)
